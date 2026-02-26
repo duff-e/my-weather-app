50323c38ca7319bf39ee831b52e9d91e3ecb991c
duff-e <elise.duffy@live.com>
2026-02-26 16:39:37 +1000

    API integration for all weather data

--- src/index.js
+++ src/index.js
@@ -14,9 +14,48 @@ function searchCity(city) {
 function updateWeather(response) {
   let updatedTemperature = document.querySelector("#current-temperature");
   let temperature = response.data.temperature.current;
-  let changeCity = document.querySelector("#current-city");
-  changeCity.innerHTML = response.data.city;
   updatedTemperature.innerHTML = Math.round(temperature);
+
+  let updatedCity = document.querySelector("#current-city");
+  updatedCity.innerHTML = response.data.city;
+
+  let updatedDescription = document.querySelector("#description");
+  updatedDescription.innerHTML = response.data.condition.description;
+
+  let updatedHumidity = document.querySelector("#humidity");
+  updatedHumidity.innerHTML = `${response.data.temperature.humidity}%`;
+
+  let updatedWind = document.querySelector("#wind");
+  updatedWind.innerHTML = `${response.data.wind.speed}m/s`;
+
+  let updatedTime = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  updatedTime.innerHTML = formatDate(date);
+
+  let updatedIcon = document.querySelector("#current-weather-icon");
+  updatedIcon.innerHTML = `<img src="${response.data.condition.icon_url}">`;
+}
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes} `;
 }
 
 let searchBarElement = document.querySelector("#search-form");
