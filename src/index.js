a896875f6627849c2e6edd4ea1f34a59766705ce
duff-e <elise.duffy@live.com>
2026-02-26 14:29:36 +1000

    added axios & search engine with API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 function searchFormElement(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let changeCity = document.querySelector("#current-city");
-  changeCity.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
-let searchCity = document.querySelector("#search-form");
-searchCity.addEventListener("submit", searchFormElement);
+function searchCity(city) {
+  let apiKey = "40o03736bbe0e6faa9b79f5dt4af0a0f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
+function updateWeather(response) {
+  let updatedTemperature = document.querySelector("#current-temperature");
+  let temperature = response.data.temperature.current;
+  let changeCity = document.querySelector("#current-city");
+  changeCity.innerHTML = response.data.city;
+  updatedTemperature.innerHTML = Math.round(temperature);
+}
+
+let searchBarElement = document.querySelector("#search-form");
+searchBarElement.addEventListener("submit", searchFormElement);
+
+searchCity("Sydney");
